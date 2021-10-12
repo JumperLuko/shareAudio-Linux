1463e79778b76b12361d3e7f5cb16f9e9faa05e5
Jumper Luko <jumper.luko@gmail.com>
2021-10-12 13:53:55 -0300

    Oh i understand now! how to move things

--- Discord-stream.sh
+++ Discord-stream.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+read "This is draft, please not run that yet"
+read "not click enter, will mess everything up"
+read "You know, more one enter and CATCHAUM"
+
 # Load Module
 #read -p "Module Name: " module
-module=Discord-Stream
-moduleID=$(pactl load-module module-null-sink sink_name=$module)
-
-
+#module=Discord-Stream
+#moduleID=$(pactl load-module module-null-sink sink_name=$module)
+pactl load-module module-null-sink sink_name=Virtual1
 
 # Create Jumper loopback input
 pactl load-module module-loopback source=alsa_input.pci-0000_08_00.4.analog-stereo sink=Virtual1
@@ -39,10 +42,17 @@ pactl load-module module-loopback source=Virtual1.monitor sink=alsa_output.pci-0
 # List IDs
 #pactl list modules | grep "Module #"
 
+# Set Playback videos (id 72) to sink_vlc
+pactl move-sink-input 72 sink_vlc
+
+# Set recording Sound Recorder (id 234) to sink_vlc.monitor
+pactl move-source-output 234 sink_vlc.monitor
+
+# Inky codes
 pactl load-module module-null-sink sink_name=sink_vlc
 pactl load-module module-null-sink sink_name=sink_jnt
 
-pactl load-module module-loopback source=microfone sink=sink_jnt latency_msec=5
+pactl load-module module-loopback source=alsa_input.pci-0000_08_00.4.analog-stereo sink=sink_jnt latency_msec=5
 pactl load-module module-loopback source=sink_vlc.monitor sink=sink_jnt latency_msec=5
 pactl load-module module-loopback source=sink_vlc.monitor sink=alsa_output.pci-0000_08_00.4.analog-stereo latency_msec=5
 
