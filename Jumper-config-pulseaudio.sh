1acb6fd7ab9003b744443ada1686b25cc98b5dfb
Jumper Luko <jumper.luko@gmail.com>
2021-11-02 11:15:25 -0300

    My config for pipewire and pulseaudio, work better

--- Jumper-config-pulseaudio.sh
+++ Jumper-config-pulseaudio.sh
@@ -1,14 +1,14 @@
 # Jumper fast work
 
 # Create virtual devices to capture audio
-pactl load-module module-null-sink sink_name=App_capture
-pactl load-module module-null-sink sink_name=AppsMic_mix
+pactl load-module module-null-sink sink_name=App_capture sink_properties=device.description="App(capture)"
+pactl load-module module-null-sink sink_name=AppsMic_mix sink_properties=device.description="AppsMic(mix)"
 #pactl load-module module-pipe-source source_name=Mic_capture
 #pactl load-module module-null-sink sink_name=null
 
 # Capture my microphone
 # My input device > AppsMic_mix
-pactl load-module module-loopback source=easyeffects_source sink=AppsMic_mix latency_msec=20
+pactl load-module module-loopback source=PulseEffects_mic.monitor sink=AppsMic_mix latency_msec=20
 
 # App_capture > AppsMic_mix
 pactl load-module module-loopback source=App_capture.monitor sink=AppsMic_mix latency_msec=20
@@ -19,8 +19,8 @@ pactl load-module module-loopback source=App_capture.monitor sink=alsa_output.pc
 
 # Transfer final sound from playback to recording
 # AppsMic_mix > AppsMic_mix-source 
-pactl load-module module-remap-source master=AppsMic_mix source_name=AppsMic_mix-source 
+pactl load-module module-remap-source master=AppsMic_mix.monitor source_name=AppsMic_mix-source 
 
 # Extra: Transfer app sound to recording
 # AppsMic_mix > AppsMic_mix-source 
-pactl load-module module-remap-source master=App_capture source_name=App_capture-source 
+pactl load-module module-remap-source master=App_capture.monitor source_name=App_capture-source 
