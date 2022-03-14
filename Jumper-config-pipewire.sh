0889b2f1484fce221800c9027d3f1be9154974e3
JumperLuko <jumper.luko@gmail.com>
2022-03-14 18:53:22 -0300

    Helvum on flathub

--- Jumper-config-pipewire.sh
+++ Jumper-config-pipewire.sh
@@ -1,9 +1,4 @@
 # Please use Helvun
-# https://gitlab.freedesktop.org/ryuukyu/helvum
+# https://flathub.org/apps/details/org.pipewire.Helvum
 
-# And use sudo for install (after compile)
-
-#sudo apt install flatpak-builder
-# Download and extrect the code, on folder run
-#flatpak install org.gnome.{Platform,Sdk}//40 org.freedesktop.Sdk.Extension.rust-stable//20.08
-#sudo flatpak-builder --install flatpak-build/ build-aux/org.freedesktop.ryuukyu.Helvum.json
+flatpak install flathub org.pipewire.Helvum
